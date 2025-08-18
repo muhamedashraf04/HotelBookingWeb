cf9d28f7859294ee0d8f7ee1a198b1cc041acc00
omar emad <omaremad11113@gmail.com>
2025-08-18 13:53:00 +0300

    Added Swagger

--- HotelBookingWeb/Program.cs
+++ HotelBookingWeb/Program.cs
@@ -1,9 +1,11 @@
 using HotelBooking.DataAccess.Data;
 using HotelBooking.DataAccess.Repos;
 using HotelBooking.Models.Models;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using HotelBooking.DataAccess.Repositories.Interfaces;
 using HotelBooking.DataAccess.Repositories;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,6 +32,8 @@ builder.Services.AddScoped<ISuiteRepository,SuiteRepository>();
 
 
 
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -38,6 +42,11 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
