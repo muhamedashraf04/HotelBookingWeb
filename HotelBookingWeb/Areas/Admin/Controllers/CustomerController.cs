b3ae6387067759cf0d02368c0ee67fb6fc37fa68
AGhaith <a.wesam2300@nu.edu.eg>
2025-08-19 15:34:21 +0300

    Created DTO for registering setup cloudinary and cloudinary server to upload images on it and save the image url in the database

--- HotelBookingWeb/Areas/Admin/Controllers/CustomerController.cs
+++ HotelBookingWeb/Areas/Admin/Controllers/CustomerController.cs
@@ -1,4 +1,7 @@
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
 using HotelBooking.DataAccess.Repositories.Interfaces;
+using HotelBooking.Models.DTOs;
 using HotelBooking.Models.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,9 +11,11 @@ public class CustomerController : Controller
 {
     private readonly ILogger<CustomerController> _logger;
     private IUnitOfWork _unitOfWork;
+    private Cloudinary _cloudinary;
 
-    public CustomerController(ILogger<CustomerController> logger, IUnitOfWork unitOfWork)
+    public CustomerController(ILogger<CustomerController> logger, IUnitOfWork unitOfWork, Cloudinary cloudinary)
     {
+        _cloudinary = cloudinary;
         _unitOfWork = unitOfWork;
         _logger = logger;
     }
@@ -22,19 +27,47 @@ public class CustomerController : Controller
     }
 
     [HttpPost]
-    public IActionResult Register([FromBody] Customer customer)
+    public IActionResult Register([FromForm] RegisterCustomerDTO registerCustomerDTO)
     {
-        if (ModelState.IsValid)
-        {
-            var today = DateOnly.FromDateTime(DateTime.Now);
-            customer.Age = today.Year - customer.BirthDate.Year; _unitOfWork.Customers.Create(customer);
-            _unitOfWork.Save();
-            return Ok("Customer Registered Successfully");
-        }
-        else
+        Customer? customer = registerCustomerDTO.customer;
+        IFormFile? IDFile = registerCustomerDTO.IdentificationFile;
+        
+        if (!ModelState.IsValid)
         {
             return BadRequest(ModelState);
+
         }
+        if (IDFile != null)
+        {
+            using var stream = IDFile.OpenReadStream();
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(IDFile.FileName, stream),
+                Folder = "hotel_booking/customers"
+            };
+
+            var uploadResult = _cloudinary.Upload(uploadParams);
+
+            if (uploadResult.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var url = uploadResult.SecureUrl.ToString();
+                customer.IdentificationAttachment = url;
+                var today = DateOnly.FromDateTime(DateTime.Now);
+                customer.Age = today.Year - customer.BirthDate.Year; _unitOfWork.Customers.Create(customer);
+                _unitOfWork.Save();
+                return Ok("Customer Registered Successfully");
+
+            }
+            else
+            {
+                if (uploadResult.Error != null)
+                    return BadRequest($"Upload failed: {uploadResult.Error.Message}");
+                return BadRequest("Failed To Upload Image");
+            }
+
+        }
+        return Ok("Customer Registered Successfully Without ID Attachment");
+
     }
 
     [HttpGet]
