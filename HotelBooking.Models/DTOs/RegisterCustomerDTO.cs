60ca0bdf394331385d47426322ea3bc4293fecfc
AGhaith <a.wesam2300@nu.edu.eg>
2025-08-19 15:34:21 +0300

    Created DTO for registering setup cloudinary and cloudinary server to upload images on it and save the image url in the database

---
+++ HotelBooking.Models/DTOs/RegisterCustomerDTO.cs
@@ -0,0 +1,17 @@
+﻿using HotelBooking.Models.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBooking.Models.DTOs
+{
+    public class RegisterCustomerDTO
+    {
+        public Customer? customer { get; set; }
+        public IFormFile? IdentificationFile { get; set; }
+        public IFormFile? MarriageCertificate { get; set; }
+    }
+}
