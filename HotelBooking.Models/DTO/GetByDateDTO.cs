b28f643568c0cc009a54bee4562b0d5b2d0b48f7
omar emad <omaremad11113@gmail.com>
2025-08-19 13:12:56 +0300

    Get Reservation by date completed

---
+++ HotelBooking.Models/DTO/GetByDateDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBooking.Models.DTO
+{
+    public class GetByDateDTO
+    {
+        public DateTime day { get; set; }
+    }
+}
