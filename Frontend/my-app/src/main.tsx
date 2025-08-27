fab479ec41d6945c3c2b9516847a2191ecdb22aa
AGhaith <a.wesam2300@nu.edu.eg>
2025-08-27 15:11:14 +0300

    Commit

--- Frontend/my-app/src/main.tsx
+++ Frontend/my-app/src/main.tsx
@@ -6,12 +6,14 @@ import Login from "./Auth/Login.tsx";
 import Register from "./Auth/Register.tsx";
 import Create from "./Customer/Create.tsx"; // Import your Create component
 import "./index.css";
-import SearchReservations from "./Reservations/SearchReservations.tsx";
 import Booking from "./Reservations/Book.tsx";
+import Checkin from "./Reservations/Checkin.tsx";
 import EditReservation from "./Reservations/EditReservation";
 import RemoveReservation from "./Reservations/Remove.tsx";
+import SearchReservations from "./Reservations/SearchReservations.tsx";
 import CreateRoom from "./Rooms/CreateRoom.tsx";
 import Edit from "./Rooms/Edit.tsx";
+import CheckInPage from "./Checkin/CheckinPage.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -28,7 +30,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <Route path="/reservation/edit/:id" element={<EditReservation />} />
         <Route path="/Register" element={<Register />} />
         <Route path="/Login" element={<Login />} />
+<Route path="/checkin/:id" element={<CheckInPage />} />
 
+        <Route path="/Reservations/Checkin" element={<Checkin />} />
         {/* Add other routes here */}
       </Routes>
     </BrowserRouter>
